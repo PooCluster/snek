a938c2a5ad7694e5786e305de71e26c3310b2f1b
PooCluster <poocluster@gmail.com>
2023-11-29 19:21:34 -0800

    Have a working snek train moving via keyboard input.

--- Engine.py
+++ Engine.py
@@ -1,6 +1,5 @@
 import pygame
 import Snek
-from OpenGL.GL import *
 
 # Engine is a static class responsible for running the core infra of the game.
 class Engine:
@@ -9,21 +8,22 @@ class Engine:
     WIDTH = 400
     HEIGHT = WIDTH / 16 * 9
     SCALE = 3
-    FPS = 60.
+    UPS = 2.
 
     # static variables
+    surface = None
     running = False
     __upsCounter = 0
     __fpsCounter = 0
 
     def initialize():
         pygame.init()
-        # set_mode(pos tuple, flags: OpenGL and double buffer rendering
-        pygame.display.set_mode((Engine.WIDTH, Engine.HEIGHT), \
-                                pygame.OPENGL|pygame.DOUBLEBUF)
+        Engine.surface = pygame.display.set_mode((Engine.WIDTH, Engine.HEIGHT), \
+                                                 pygame.DOUBLEBUF)
+
         pygame.display.set_caption(Engine.TITLE)
 
-        Engine.snek = Snek.Snek(Engine.WIDTH / 2, Engine.HEIGHT / 2)
+        Engine.snek = Snek.Snek(Engine.surface, int(Engine.WIDTH / 2), int(Engine.HEIGHT / 2))
 
         Engine.running = True
         return Engine.running
@@ -32,22 +32,21 @@ class Engine:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 Engine.running = False
-        # TODO
+        Engine.snek.update()
 
     def __render():
-        glClearColor(1, 1, 1, 1) # RGBA
-        glClear(GL_COLOR_BUFFER_BIT)
+        Engine.surface.fill((0, 0, 0))
 
         # TODO: game rendering stuff here
         Engine.snek.render()
 
-        # swap to other buffer
+        # update and swap to other buffer
         pygame.display.flip()
 
     def gameLoop():
         time = pygame.time.get_ticks()
         previousTime = time
-        interval = 1000. / Engine.FPS
+        interval = 1000. / Engine.UPS
         difference = 0.
         while Engine.running:
             currentTime = pygame.time.get_ticks()
@@ -60,7 +59,7 @@ class Engine:
             Engine.__render()
             Engine.__fpsCounter += 1
             if currentTime - time >= 1000:
-                # TODO: 1 second has past, update display
+                # 1 second has past, update display
                 time += 1000
                 info = str(Engine.__fpsCounter) + " FPS " + \
                        str(Engine.__upsCounter) + " UPS"
--- snek.py
+++ snek.py
@@ -1,23 +1,56 @@
 import pygame
+from enum import Enum
 
 class Snek:
+    class Direction(Enum):
+        UP    = 0
+        DOWN  = 1
+        LEFT  = 2
+        RIGHT = 3
+
     class SnekNode:
         def __init__(self, x, y):
-            self.pos = (x, y)
+            self.x = x
+            self.y = y
 
-    def __init__(self, x = -1, y = -1):
-        self.chain = [                  \
-            Snek.SnekNode(x, y),        \
-            Snek.SnekNode(x + 1, y),    \
-            Snek.SnekNode(x + 1, y + 1) \
+    def __init__(self, surface, x = -1, y = -1):
+        self.chain = [                \
+            Snek.SnekNode(x, y),      \
+            Snek.SnekNode(x + 16, y), \
+            Snek.SnekNode(x + 32, y)  \
         ]
+        self.surface = surface
+        self.direction = Snek.Direction.LEFT
 
     def update(self):
-        # TODO
-        print("TODO")
+        # read direction
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and not self.direction == Snek.Direction.DOWN:
+            self.direction = Snek.Direction.UP
+        if keys[pygame.K_DOWN] and not self.direction == Snek.Direction.UP:
+            self.direction = Snek.Direction.DOWN
+        if keys[pygame.K_LEFT] and not self.direction == Snek.Direction.RIGHT:
+            self.direction = Snek.Direction.LEFT
+        if keys[pygame.K_RIGHT] and not self.direction == Snek.Direction.LEFT:
+            self.direction = Snek.Direction.RIGHT
+
+        # calculate translations (origin is based on top-left)
+        xChange = 0
+        yChange = 0
+        if self.direction == Snek.Direction.UP:
+            yChange -= 16
+        if self.direction == Snek.Direction.DOWN:
+            yChange += 16
+        if self.direction == Snek.Direction.LEFT:
+            xChange -= 16
+        if self.direction == Snek.Direction.RIGHT:
+            xChange += 16
+
+        # move head to new location
+        self.chain.insert(0, Snek.SnekNode(self.chain[0].x + xChange, self.chain[0].y + yChange))
+        # remove tail
+        self.chain.pop()
 
     def render(self):
-        # TODO
         for snekNode in self.chain:
-            surface = pygame.Surface([16, 16])
-            pygame.Surface.fill(surface, (0, 255, 0))
+            pygame.draw.rect(self.surface, (0, 255, 0, 255), pygame.Rect(snekNode.x, snekNode.y, 16, 16))
